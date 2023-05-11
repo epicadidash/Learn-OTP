fee848fe031643643c6dc869411d0553edd0eba4
epicadidash <dradwaitdash@gmail.com>
2023-05-11 23:54:46 +0530

    Creation of Gen Server functions Natively to learn how they work behind the scenes

--- lib/counter.ex
+++ lib/counter.ex
@@ -1,18 +1,14 @@
 defmodule Counter do
-  @moduledoc """
-  Documentation for `Counter`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Counter.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def start(initial_count) do
+  spawn( fn() -> Counter.Server.run(initial_count) end )
   end
-end
+  def tick(pid) do
+  send pid, {:tick, self()}
+  end
+  def state(pid) do
+  send pid, {:state, self()}
+  receive do
+  {:count, value} -> value
+  end
+  end
+  end
